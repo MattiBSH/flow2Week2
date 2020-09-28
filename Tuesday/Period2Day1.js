308086c301413c1a34626b0939c27901c2f1298b
MattiBSH <mattibenhansen@gmail.com>
2020-09-28 14:21:46 +0200

    first commit

---
+++ Tuesday/Period2Day1.js
@@ -0,0 +1,158 @@
+/* 1 */
+/* a */
+var names = ["diane", "anne", "millie", "bent"];
+ function filterByLetter(arr) {
+  return arr.filter(function(el) {
+      return el.toLowerCase().indexOf('a') !== -1
+  })
+}
+
+console.log(filterByLetter(names));
+  /* b */
+  function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+function reverseAll(){
+var reversed=names.map(name=>{
+    return reverseString(name);
+});
+return reversed;
+}
+console.log(reverseAll());
+/* 2 */
+
+function myFilter(array, callback){
+    return callback(array);
+}
+console.log(myFilter(names,filterByLetter));
+
+function myMap(array, callback){
+  return callback(array);
+}
+console.log(myFilter(names,reverseAll));
+
+/** 3/ */
+/* a */
+var numbers = [1, 3, 5, 10, 11];
+
+var n=0;
+function changenumbers(array){
+  
+   var numbers=array.map(number=>{
+     n++;
+    if(n<(array.length)){
+      return (number+array[n]);
+    }
+    else{
+      return number;
+    }});
+  return numbers;
+}
+console.log(myFilter(numbers,changenumbers));
+/* b */
+var names2=["Matti","Nikolaj","Vibeke","Mathias"];
+
+function getA(){
+  var s="";
+names2.map(name=>{
+s += "<a href=””>"+name+"</a>"
+});
+return s;
+}
+var strr="<nav>"+getA()+"</nav>";
+
+console.log(strr);
+/* c */
+var persons = [{name:"Hassan",phone:"1234567"}, {name: "Peter",phone: "675843"}, {name: "Jan", phone: "98547"},{name: "Boline", phone: "79345"}];
+function makeTable(){
+  var s =persons.map(human=>"<tr><th>"+human.name+"</th><th>"+human.name+"</th></tr>").join("");
+  return "<table>"+s+"</table>";
+}
+console.log("__________________________");
+console.log("make table");
+console.log(makeTable());
+/*4*/
+var all= ["Hassan", "Peter", "Carla", "Boline"];
+var strr2=all.join("#");
+console.log(strr2);
+/* b*/
+var numbers2 = [2, 3, 67, 33]; 
+
+const sum = numbers2.reduce((total, amount) => total + amount); 
+console.log(sum);
+
+var members = [
+  {name : "Peter", age: 18},
+  {name : "Jan", age: 35},
+  {name : "Janne", age: 25},
+  {name : "Martin", age: 22}];
+/*c*/ 
+  var average = members.reduce((accumulator, member,index,arr) => {
+    accumulator += member.age
+   
+    return accumulator/arr.length
+  }, 0)
+  console.log(average);
+/**d */
+const votes = [ "Biden","Trump","Biden","Biden","Trump","Trump","Biden","None"];
+
+var count = function(array) {
+  return votes.reduce(function(counter, item) {
+      var p = (item);
+      counter[p] = counter.hasOwnProperty(p) ? counter[p] + 1 : 1;
+      return counter;
+  }, {})
+}
+
+console.log(count(votes));
+
+
+  /**5 */
+  /**a 
+   * The console.log() should be made outside the class
+   * So the return type should be a variable and not a console.log();
+  */
+
+  /**b
+   * same reason as before
+   *  * The console.log() should be made outside the class
+   * So the return type should be a variable and not a console.log();
+   */
+
+  // Initiate counter
+var counter = 0;
+
+// Function to increment counter
+function add() {
+  counter += 1;
+}
+
+// Call add() 3 times
+add();
+add();
+add();
+
+// The counter should now be 3
+console.log(counter);
+
+var person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  getInfo : function() {
+    return this.firstName + " " + this.lastName;
+  },
+  setName : function(name) {
+    this.firstName=name;
+    return this.firstName;
+  },
+  setLName : function(name) {
+    this.lastName=name;
+    return this.lastName;
+  }
+}
+
+console.log(person.getInfo());
+console.log(person.setName("Bob"));
+console.log(person.setLName("Bent"));
+console.log(person.getInfo());
